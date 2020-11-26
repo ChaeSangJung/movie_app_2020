bf963ef1bf164665f06d8a3806218d23f7296f1f
Chae Sang, Jung <shtingstar@hanmail.net>
2020-11-26 23:12:46 +0900

    Updates

---
+++ precache-manifest.71400a4fd578ef31b1e97b550174acef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0a99f1f59547b888a4edc79535a29c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a91b6c1f687e1ce32706",
+    "url": "/movie_app_2020/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "39025343668124452491",
+    "url": "/movie_app_2020/static/js/2.2a1b83cb.chunk.js"
+  },
+  {
+    "revision": "a91b6c1f687e1ce32706",
+    "url": "/movie_app_2020/static/js/main.be9185ea.chunk.js"
+  },
+  {
+    "revision": "114d3434d006dc29083b",
+    "url": "/movie_app_2020/static/js/runtime~main.6c3fa8d1.js"
+  }
+]);
